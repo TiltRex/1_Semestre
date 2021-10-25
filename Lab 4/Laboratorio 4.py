56419152775c1ca6b455ae9f0b16e50101cba616
Andrés León <88801431+TiltRex@users.noreply.github.com>
2021-10-25 17:43:02 -0500

    Add files via upload

---
+++ Lab 4/Laboratorio 4.py
@@ -0,0 +1,26 @@
+## 2.1
+num1= int(input("Ingrese un numero entero: "))
+num_2 = num1**2
+num_3 = num1**3
+
+print("El cuadrado de",num1,"es:",num_2)
+print("El cubo de",num1,"es:",num_3)
+
+print("-----------------------------------------")
+
+##2.4
+pi= 3.141592
+r = float(input("Indroduzca el valor del radio: "))
+h = float(input("Introduzca el valor de la altura: "))
+V = float(pi * r**2)*h
+
+print("El volumen es:",V)
+
+print("-----------------------------------------")
+
+##2.6
+gr= float(input("Introduzca el peso en gramos: "))
+kg = float(gr/1000)
+
+print("El valor en kilogramos es:",kg)
+
